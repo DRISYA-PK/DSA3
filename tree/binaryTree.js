1d5de83b4ce10dc84cd37d404d746257917ba512
Drisya PK <drisyapk68@gmail.com>
2025-04-02 12:13:55 +0530

    new function updated

--- tree/binaryTree.js
+++ tree/binaryTree.js
@@ -11,8 +11,9 @@ class BinaryTree{
     constructor()
     {
         this.root=null;
+        this.display=" ";
     }
-    Intersert(value)
+    Insert(value)
     {
         const newNode=new Node(value);
 
@@ -21,7 +22,7 @@ class BinaryTree{
             this.root=newNode;
         }
         else{
-            InsertValue(this.root,newNode)
+            this.InsertValue(this.root,newNode)
         }
 
     }
@@ -47,10 +48,97 @@ class BinaryTree{
                 }
         }
     }
+    search(value)
+    {
+        if(this.root===null)
+        {
+            return "No value";
+        }
+        let current=this.root;
+       // this.search
+        while(current)
+        {
+            if(current.value===value)
+            {
+                return "value found";
+            }
+            else if(current.value>value)
+            {
+                current=current.left;
+
+            }
+            else{
+                current=current.right;
+            }
+        }
+        return "No node found";
+
+    }
+preOrderTraversal(node )
+{
+    if(!node) return ;
+    console.log(node.value);
+   // this.display +=" "+node.value;
+    this.preOrderTraversal(node.left);
+    this.preOrderTraversal(node.right);
+   
+    
+}
+inOrderTraversal(node)
+{
+    if(!node) return ;
+    this.inOrderTraversal(node.left);
+
+    console.log(node.value)
+    this.inOrderTraversal(node.right);
 }
 
 
-const node=new Node();
-node.insertBefore(100);
-node.insertBefore(200);
-node.Intersert(30);
+postOrderTraversal(node)
+{
+    if(!node) return;
+    this.postOrderTraversal(node.left);
+    this.postOrderTraversal(node.right)
+    console.log(node.value);
+}
+
+
+bfs()
+{
+    let queue=[];
+    queue.push(this.root);
+    while(queue.length)
+    {
+      let curr=  queue.shift();
+      console.log(curr.value);
+      if(curr.left)
+      {
+        queue.push(curr.left);
+      }
+       if(curr.right)
+       {
+        queue.push(curr.right)
+       }
+
+    }
+}
+
+}
+
+
+const node=new BinaryTree();
+node.Insert(10);
+node.Insert(5);
+node.Insert(3);
+node.Insert(7);
+node.Insert(15)
+console.log(node.search(30))
+console.log(node.search(130))
+node.preOrderTraversal(node.root)
+console.log("-----------------------")
+
+node.inOrderTraversal(node.root)
+console.log("-----------------------")
+node.postOrderTraversal(node.root)
+console.log("-----------------------")
+node.bfs();
